6ea4127c04c85f95a4d23cea250136908181779b
Martin <martoxxx100@gmail.com>
2021-08-15 01:14:00 -0300

    pintando submenus

--- aperitivos.js
+++ aperitivos.js
@@ -1,3 +1,63 @@
+function getContentful(){
+return  fetch(" https://cdn.contentful.com/spaces/uq7529l1n1cl/environments/master/entries?access_token=BIKLyw8bHobEupEDPoOGm27ZVh9Iqi7LQfA5s313as4")
+.then(response => response.json())
+.then(data => {
+    var subMenu = data.items.map((items)=>{
+        return{
+            subMenu:items.fields.tipoDeSubMenu
+        }
+    })
+    var productos = data.items.map((items)=>{
+        return{
+                clase:items.fields.clase,
+                tipo:items.fields.tipo,
+                titulo:items.fields.titulo,
+                descripcion: items.fields.descripcion,
+                precio:items.fields.precio
+        }
+    })
+   return [subMenu, productos]
+});
+
+}
+
+//agrega los submenus 
+
+function agregarSubMenu(data){
+   const contenedor = document.querySelector(".menu__aperitivos")
+   const template = document.querySelector(".template").content
+   var titulos = template.querySelector(".menu__aperitivos-sub--titulo")
+   titulos.textContent = data.subMenu
+ 
+    const clone = template.cloneNode(true)
+    contenedor.appendChild(clone)
+}
+
+//agrega los productos
+function agregarProductos(data){
+console.log(data)
+
+}
+
+// evita que el orden de los sub-menus se modifique despues de hacer cambios en contentful
+
+function Ordenar(submenu,){
+    const listaOrdenada = submenu.sort(function(a, b) {
+        var nameA = a.tipo.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.tipo.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      
+        return 0;
+      });
+      return listaOrdenada
+}
+
+
 function mainMenu(){
 crearNav();
 crearUbicacion()
@@ -6,6 +66,20 @@ const botonMenu = document.querySelector(".header-icono")
 const menuOculto = document.querySelector(".header-nav")
 const contenedor = document.querySelector(".container")
 
+//envia la data de contentful para crear los sub menus y productos
+getContentful().then((submenu)=>{
+        for (const s of submenu[0]){
+            if (s.subMenu !== undefined){
+                 agregarSubMenu(s)
+            }
+        }
+        for (const p of submenu[1]){
+            if (p.clase !== undefined){
+                 agregarProductos(p)
+            }
+        }
+
+})
 
 //botones menu oculto mobile
 botonMenu.addEventListener("click",(e)=>{
--- index.js
+++ index.js
@@ -1,59 +1,3 @@
-function getContentful(){
-return  fetch(" https://cdn.contentful.com/spaces/uq7529l1n1cl/environments/master/content_types?access_token=BIKLyw8bHobEupEDPoOGm27ZVh9Iqi7LQfA5s313as4")
-.then(response => response.json())
-.then(data => {
-    const subMenu = data.items.map((items)=>{
-        return{
-            tipo:items.name,
-            descripcion: items.description
-        }
-    })
-    return subMenu
-});
-
-}
-
-//agrega los submenus y los hrefs de los links
-function agregarSubMenu(data){
-const contenedor = document.querySelector(".menu-container__cards")
-const template = document.querySelector(".submenu").content
-const titulo = template.querySelector(".card-h4")
-const descripcion = template.querySelector(".card__descripcion")
-const img = template.querySelector(".card__img")
-const a = template.querySelector(".a")
-const linkDescktop = template.querySelector(".card__link-full")
-
-a.href = "./" + data.tipo +".html"
-titulo.textContent = data.tipo
-descripcion.textContent = data.descripcion;
-img.src = `./imagenes/${data.tipo.toLowerCase()}.jpg`
-linkDescktop.href = "/" + data.tipo 
-
-
-const clone = template.cloneNode(true)
-contenedor.appendChild(clone)
-return contenedor
-}
-
-// evita que el orden de los sub-menus se modifique despues de hacer cambios en contentful
-function Ordenar(submenu){
-    const listaOrdenada = submenu.sort(function(a, b) {
-        var nameA = a.tipo.toUpperCase(); // ignore upper and lowercase
-        var nameB = b.tipo.toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-      
-        return 0;
-      });
-      return listaOrdenada
-}
-
-
-
 function main(){
 crearCarousel()
 crearNav()
@@ -63,14 +7,6 @@ const botonMenu = document.querySelector(".header-icono")
 const menuOculto = document.querySelector(".header-nav")
 const contenedor = document.querySelector(".container")
 
-//envia la data de contentful para crear los sub menus
-getContentful().then((submenu)=>{
- 
-    Ordenar(submenu)
-        for (const r of submenu){
-            agregarSubMenu(r)
-        }
-})
 //botones menu oculto mobile
 botonMenu.addEventListener("click",(e)=>{
     e.stopPropagation()
