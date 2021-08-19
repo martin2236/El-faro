785ba1299ce614735d8da898bb3d01de0a3095f6
Martin <martoxxx100@gmail.com>
2021-08-19 11:42:54 -0300

    menu mobile

--- footer-comp/footer.js
+++ footer-comp/footer.js
@@ -7,22 +7,29 @@ function crearFooter(){
     <div class="footer-contacto">
     <h2 class="footer-contacto__h2">Contacto</h2>
 <div class="footer-contacto__telefonos">
-    <h2 class="footer-contacto__h3">Telefonos</h3>
-    <p class="footer-contacto__div--link">Tel : 02257-60473</p>
-    <p class="footer-contacto__div--link">Tel : 02257-12341</p>
+    <h2 class="footer-contacto__telefono-h3">Telefonos</h3>
+    <img class="footer-contecto__telefono-icon" src="./imagenes/telefono.png" alt="icono">
+    <p class="footer-contacto__telefono-link"> Tel : 02257-60473</p>
+    <br>
+    <img class="footer-contecto__telefono-icon" src="./imagenes/whatsapp.png" alt="icono">
+    <p class="footer-contacto__telefono-link"> Tel : 02257-12341</p>
 </div>    
 <div class="footer-contacto__redes">
-    <h2 class="footer-contacto__h3">redes</h3>
-    <p class="footer-contacto__div--link">Insta : @CafeteriaElFaro</p>
-    <p class="footer-contacto__div--link">Face :ElFaroCafeteria</p>
+    <h2 class="footer-contacto__redes-h3">redes</h3>
+    <img class="footer-contecto__redes-icon" src="./imagenes/instagram.png" alt="icono">
+    <p class="footer-contacto__redes-link">Insta : @CafeteriaElFaro</p>
+    <br>
+    <img class="footer-contecto__redes-icon" src="./imagenes/facebook.png" alt="icono">
+    <p class="footer-contacto__redes-link">Face :ElFaroCafeteria</p>
 </div>    
 </div>
     <div class="footer-horarios">
-        <h2 class="footer-horarios__h2">Horarios</h2>
-    <div class="footer-horarios__div">
-        <p class="footer-horarios__div--dias">De Lunes a Lunes</p>
-    <p class="footer-horarios__div--hs">De 7 AM a 3 AM</p>
-    </div>
+    <h3 class="footer-horarios__h3">Horarios</h3>
+    <img class="footer-contecto__icon" src="./imagenes/horario.png" alt="icono">
+    <p class="footer-horarios__horario">De Lunes a Lunes</p>
+    <br>
+    <p class="footer-horarios__horario">De 7 AM a 3 AM</p>
+  
 </div>
     `
     contenedor.appendChild(footer)
